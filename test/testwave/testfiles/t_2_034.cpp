4af3d44f67413b8cea2eea541d28939fd521fa90
Martin Medler <36563496+martis42@users.noreply.github.com>
2026-09-20 18:21:40 +0200

    Add tests for aliasing _has_include with custom macros

---
+++ test/testwave/testfiles/t_2_034.cpp
@@ -0,0 +1,34 @@
+/*=============================================================================
+    Boost.Wave: A Standard compliant C++ preprocessor library
+    http://www.boost.org/
+
+    Copyright (c) 2026 Martin Medler. Distributed under the Boost
+    Software License, Version 1.0. (See accompanying file
+    LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
+// An object like macro can be used as alias to the special macro __has_include
+
+//O --c++17
+//O -Werror
+
+// point system path to this directory
+//O -S.
+
+#define HAS_INCLUDE __has_include
+#if HAS_INCLUDE(<t_2_034.cpp>)
+#define FOUND_SELF_VIA_ALIAS
+#else
+#warning has_include could not find this file through an alias
+#endif
+
+//H 10: t_2_034.cpp(18): #define
+//H 08: t_2_034.cpp(18): HAS_INCLUDE=__has_include
+//H 10: t_2_034.cpp(19): #if
+//H 01: t_2_034.cpp(18): HAS_INCLUDE
+//H 02: __has_include
+//H 03: __has_include
+//H 11: t_2_034.cpp(19): #if HAS_INCLUDE(<t_2_034.cpp>): 1
+//H 10: t_2_034.cpp(20): #define
+//H 08: t_2_034.cpp(20): FOUND_SELF_VIA_ALIAS=
+//H 10: t_2_034.cpp(21): #else
